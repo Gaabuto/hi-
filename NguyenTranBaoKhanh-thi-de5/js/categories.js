f87b7895892ebc2c20e0b22675c69692dd92efa6
Gaabuto <khanh040604@gmail.com>
2025-04-07 16:27:17 +0700

    categories.js

--- NguyenTranBaoKhanh-thi-de5/js/categories.js
+++ NguyenTranBaoKhanh-thi-de5/js/categories.js
@@ -1,32 +1,46 @@
 let categories = JSON.parse(localStorage.getItem("categories")) || [];
+let editIndex = -1;
 
 function addNewCategory(e) {
   e.preventDefault();
   let nameCategory = document.getElementById("addName").value;
-  if (nameCategory == "") {
-    alert("Bạn cần đặt tên cho category");
+  let description = document.getElementById("description").value;
+  if (editIndex == -1) {
+    document.getElementById("exampleModalLabel").innerText = "Add Category";
+    if (nameCategory == "") {
+      alert("Bạn cần đặt tên cho category");
+    } else if (
+      categories.filter(
+        (category) =>
+          category.nameCategory.toLowerCase() === nameCategory.toLowerCase()
+      ).length > 0
+    ) {
+      alert("Tên không được trùng");
+    } else {
+      let newCategory = {
+        nameCategory: nameCategory,
+        description: description,
+      };
+      categories.push(newCategory);
+      localStorage.setItem("categories", JSON.stringify(categories));
+    }
   } else {
-    let description = document.getElementById("description").value;
+    document.getElementById("exampleModalLabel").innerText = "Edit Category";
+    categories[editIndex].nameCategory = nameCategory;
+    categories[editIndex].description = description;
 
-    let newCategory = {
-      nameCategory: nameCategory,
-      description: description,
-    };
-
-    categories.push(newCategory);
-    localStorage.setItem("categories", JSON.stringify(categories));
-    render();
-
-    nameCategory.value = "";
-    description.value = "";
+    editIndex = -1;
+    document.getElementById("exampleModalLabel").innerText = "Add Category";
   }
+
+  render();
+  document.getElementById("addName").value = "";
+  document.getElementById("description").value = "";
 }
 
 function render() {
   let body = document.getElementById("tbody");
   body.innerHTML = "";
-  document.getElementById("exampleModalLabel").innerText = "Add Category";
-
   categories.forEach((category, index) => {
     body.innerHTML += `
       <tr style="background-color: white;">
@@ -37,14 +51,23 @@ function render() {
           <button onclick="delCategory(${index})" style="border: none; color: #CA533B; background-color: white;">delete</button>
         </td>
       </tr>
+      
     `;
   });
 }
 
 function delCategory(index) {
-  categories.splice(index, 1);
+  if (confirm()) categories.splice(index, 1);
   localStorage.setItem("categories", JSON.stringify(categories));
   render();
+}
+function editCategory(index) {
+  editIndex = index;
+  document.getElementById("addName").value = categories[index].nameCategory;
+  document.getElementById("description").value = categories[index].description;
+
+  let modal = new bootstrap.Modal(document.getElementById("exampleModal"));
+  modal.show();
 }
 
 render();
